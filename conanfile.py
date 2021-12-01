7f9dd2ed4190c3e995732a1060b123f6efeb9b52
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2021-12-01 09:11:16 +0100

    Replacing conanfile.txt with conanfile.py

---
+++ conanfile.py
@@ -0,0 +1,21 @@
+from conans import ConanFile, CMake
+
+
+class Anote(ConanFile):
+    settings = "os", "compiler", "build_type", "arch"
+    requires = ["wxwidgets/3.1.4@terranum-conan+wxwidgets/stable"]
+
+    generators = "cmake", "gcc", "txt"
+
+    def configure(self):
+        if self.settings.os == "Linux":
+            self.options["wxwidgets"].webview = False # webview control isn't available on linux.
+
+    def imports(self):
+        self.copy("*.dll", dst="bin", src="bin")  # From bin to bin
+        self.copy("*.dylib*", dst="bin", src="lib")  # From lib to bin
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
