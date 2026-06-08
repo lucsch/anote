ef33f5d35baa9b137f8a098f952535dde8678b3e
Lucien SCHREIBER <lucien.schreiber@terranum.ch>
2026-06-08 14:46:41 +0200

    Removing secretstore option from wxWidgets build (too old in 22.04)

--- conanfile.py
+++ conanfile.py
@@ -7,14 +7,15 @@ class AnoteRecipe(ConanFile):
     version = "3.1"
     settings = "os", "compiler", "build_type", "arch"
     generators = "CMakeDeps", "CMakeToolchain"
+    default_options = {
+        "wxwidgets/*:secretstore": False,
+    }
 
     def requirements(self):
         self.requires("wxwidgets/3.3.2")
         if self.settings.os == "Linux":
             # conflict between wxwidgets 3.3.2 and libtiff/4.6.0
             self.requires("libwebp/1.6.0", override=True)
-            # Force l'utilisation d'une version récente de libgpg-error requise par libgcrypt
-            self.requires("libgpg-error/1.61", override=True)
 
     def layout(self):
         cmake_layout(self)
